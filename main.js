9cd68c92d010a159ca8d1c690eb8c6a0a0f55aba
Samuel1242 <94230313+Samuel1242@users.noreply.github.com>
2022-07-13 09:13:47 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+Peter_pan_song="";
+Harry_potter_theme_song="";
+
+function setup(){
+    canvas = createCanvas(600,600);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+}
+
+function preload(){
+    Peter_pan_song = loadSound("music2.mp3");
+    Harry_potter_theme_song = loadSound("music.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,600);
+}
